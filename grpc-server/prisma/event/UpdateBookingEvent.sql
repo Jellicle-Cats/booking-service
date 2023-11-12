468cf105eab60c72883a33a971cc67c69c5d668c
Ryn Parin <ryn.parin@gmail.com>
2023-11-12 18:40:13 +0700

    feat: add update completed status

--- grpc-server/prisma/event/UpdateBookingEvent.sql
+++ grpc-server/prisma/event/UpdateBookingEvent.sql
@@ -1,10 +1,23 @@
-CREATE EVENT UpdateBookingStatusEvent
-ON SCHEDULE EVERY 1 MINUTE  -- Adjust the frequency as needed
+-- Update status to 4 (CANCELLED) after 15 minutes of scheduled start time for unchecked-in bookings
+CREATE EVENT UpdateUncheckedinBookingStatusEvent
+ON SCHEDULE EVERY 1 MINUTE
 DO
 BEGIN
   UPDATE Booking
   SET status = 4
-  WHERE status IN (1)
+  WHERE status = 1
     AND checkinTime IS NULL
     AND startTime + INTERVAL 15 MINUTE < NOW();
+END;
+
+-- Update status to 3 (COMPLETED) when the current time is greater than the end time for checked-in bookings
+CREATE EVENT UpdateCompletedBookingStatusEvent
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+  UPDATE Booking
+  SET status = 3
+  WHERE status = 2
+    AND checkinTime IS NOT NULL
+    AND endTime < NOW();
 END;
